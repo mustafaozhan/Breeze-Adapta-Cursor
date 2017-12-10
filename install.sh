c2eb7d3e35bed5f405b3e6c448676920703942ab
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2017-12-10 23:47:15 +0300

    install.sh modified

--- install.sh
+++ install.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+cd Breeze-Adapta;
+sudo mkdir /usr/share/icons/Breeze-Adapta;
+sudo mv cursors/ /usr/share/icons/Breeze-Adapta;
+sudo mv cursor.theme /usr/share/icons/Breeze-Adapta;
+sudo mv index.theme /usr/share/icons/Breeze-Adapta;
+sudo mv README.md /usr/share/icons/Breeze-Adapta;
